1c0767a88273ebf2bd86720c5e2130c2ed22140b
Luz <ladsan@hotmail.com>
2024-10-25 13:10:04 -0400

    Final commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,20 +8,30 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json = input_json, headers = headers)
 
-    # Format the response in a JSON format
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    if response.status_code  == 200:
+        # Format the response in a JSON format
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    # Get dominant emotion
-    dominant_emotion = max(emotions, key=emotions.get)
+         # Get dominant emotion
+        dominant_emotion = max(emotions, key=emotions.get)
 
-    result = {
-        'anger': emotions['anger'],
-        'disgust': emotions['disgust'],
-        'fear': emotions['fear'],
-        'joy': emotions['joy'],
-        'sadness': emotions['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+        result = {
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    elif response.status_code  == 400:
+        result = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     return result
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +9,22 @@ app = Flask('Emotion Detection')
 
 @app.route('/emotionDetector')
 def sent_emotion():
-    
+
     # Get the text from the arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the function
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     return f"""For the given statement, the system response is 
-    'anger': {response['anger']}, 
-    'disgust': {response['disgust']}, 
-    'fear': {response['fear']}, 
-    'joy': {response['joy']} and 'sadness': {response['sadness']}. 
-    The dominant emotion is {response['dominant_emotion']}."""
+        'anger': {response['anger']}, 
+        'disgust': {response['disgust']}, 
+        'fear': {response['fear']}, 
+        'joy': {response['joy']} and 'sadness': {response['sadness']}. 
+        The dominant emotion is {response['dominant_emotion']}."""
 
 @app.route("/")
 def render_index_page():
@@ -25,7 +32,4 @@ def render_index_page():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
-
-
+    
